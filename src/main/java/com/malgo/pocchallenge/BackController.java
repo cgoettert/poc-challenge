38d05315dfd49b4f18a83e2eb54553491b37f186
Charles Goettert <8600088+cgoettert@users.noreply.github.com>
2022-08-26 13:32:27 -0300

    get to post

--- src/main/java/com/malgo/pocchallenge/BackController.java
+++ src/main/java/com/malgo/pocchallenge/BackController.java
@@ -1,13 +1,13 @@
 package com.malgo.pocchallenge;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BackController {
 
-    @GetMapping("/api/data")
+    @PostMapping("/api/data")
     public ResponseEntity<SomeData> someData() {
         return ResponseEntity.ok(new SomeData());
     }
